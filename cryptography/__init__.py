bfb655c37b3c40e64761f2bfbfb457b38aa55141
NikonorovDS <kennyqlolol@gmail.com>
2021-11-18 11:54:27 +0300

    Create __init__.py

---
+++ cryptography/__init__.py
@@ -0,0 +1,19 @@
+from .functions import del_string
+
+from .functions import key_lenght
+
+from .functions import frq
+
+from .functions import xor_strings
+
+from .functions import string_chr
+
+from .functions import xor_strings_nochar
+
+from .functions import str_elements
+
+from .functions import xor_string_chr
+
+from .functions import spiski_vvod
+
+from .input import *
